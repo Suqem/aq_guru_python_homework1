a6f47d9adcd6bf276a030c868a1995e67b5cb551
Михаил <sidorkin-misha93@mail.ru>
2023-11-17 17:24:22 +0300

    Длинна и площадь сделал.

--- test_homework.py
+++ test_homework.py
@@ -1,3 +1,4 @@
+import math
 
 
 def test_greeting():
@@ -37,12 +38,13 @@ def test_circle():
     """
     r = 23
     # TODO сосчитайте площадь
-    area = 0
+    area = math.pi * r ** 2
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
-    length = 0
+    length = 2 * math.pi * r
     assert length == 144.51326206513048
+    print(f"\nДлина круга равна {length}. Площадь круга равна {area}.")
 
 
 def test_random_list():
